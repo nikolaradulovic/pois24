81b080c1af3057b2328e548f00f2f3492c635c5e
phillipoJavniTuzilac <phillipo.992@gmail.com>
2014-05-28 16:53:53 +0200

    Full komit, povezane klase, baguje layout kad se pokrene u emulatoru

--- pois24/src/com/example/pois24/MainActivity.java
+++ pois24/src/com/example/pois24/MainActivity.java
@@ -3,20 +3,70 @@ package com.example.pois24;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
+
+	Button sat, poruka, stetoskop, poziv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		sat = (Button) findViewById(R.id.btnSat);
+		poruka = (Button) findViewById(R.id.btnPoruka);
+		stetoskop = (Button) findViewById(R.id.btnStetoskop);
+		poziv = (Button) findViewById(R.id.btnPoziv);
+
+		sat.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openSatActivity = new Intent("com.example.pois24.SAT");
+				startActivity(openSatActivity);
+
+			}
+		});
+
+		poruka.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openPorukaActivity = new Intent("com.example.pois24.PORUKA");
+				startActivity(openPorukaActivity);
+			}
+		});
+
+		stetoskop.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openStetoskopActivity = new Intent("com.example.pois24.STETOSKOP");
+				startActivity(openStetoskopActivity);
+			}
+		});
+
+		poziv.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openPozivActivity = new Intent("com.example.pois24.POZIV");
+				startActivity(openPozivActivity);
+			}
+		});
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
