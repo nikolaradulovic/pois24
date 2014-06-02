a3e30f827f6b1ff2015cc00d6e73134fb96c8253
phillipoJavniTuzilac <phillipo.992@gmail.com>
2014-06-02 18:52:43 +0200

    Odradjene poruke.

--- pois24/src/com/example/pois24/PorukaActivity.java
+++ pois24/src/com/example/pois24/PorukaActivity.java
@@ -3,26 +3,77 @@ package com.example.pois24;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.os.Build;
 
 public class PorukaActivity extends ActionBarActivity {
+	
+	Button back;
+	ListView lista;
+	
+	String[] poruke = new String[] { "poruka1", "poruka2", "poruka3", "poruka4", "poruka5", };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_poruka);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		back = (Button) findViewById(R.id.btnBack);
+		
+		ArrayAdapter adapter = new ArrayAdapter<String>(this,
+				R.layout.activity_listview, poruke);
+
+		lista = (ListView) findViewById(R.id.listaPoruka);
+		lista.setAdapter(adapter);
+
+		lista.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				
+				switch (position) {
+				case 0:
+					Intent openPoruka1 = new Intent("com.example.pois24.PORUKA1");
+					startActivity(openPoruka1);
+					break;
+				case 1:
+					Intent openPoruka2 = new Intent("com.example.pois24.PORUKA2");
+					startActivity(openPoruka2);
+					break;		
+				case 2:	
+					Intent openPoruka3 = new Intent("com.example.pois24.PORUKA3");
+					startActivity(openPoruka3);
+					break;
+				case 3:
+					Intent openPoruka4 = new Intent("com.example.pois24.PORUKA4");
+					startActivity(openPoruka4);
+					break;
+				case 4:
+					Intent openPoruka5 = new Intent("com.example.pois24.PORUKA5");
+					startActivity(openPoruka5);
+					break;
+				}
+			}
+		});
+		
 	}
+
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,23 +93,6 @@ public class PorukaActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_poruka,
-					container, false);
-			return rootView;
-		}
 	}
 
 }
---
+++ pois24/src/com/example/pois24/SendPoruku2.java
@@ -0,0 +1,64 @@
+package com.example.pois24;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SendPoruku2 extends Activity {
+
+	Button buttonSend, nazad;
+	EditText textPhoneNo;
+	EditText textSMS;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.poruka2);
+
+		nazad = (Button) findViewById(R.id.btnBack);
+		buttonSend = (Button) findViewById(R.id.buttonSend);
+		textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+		textSMS = (EditText) findViewById(R.id.editTextSMS);
+
+		buttonSend.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				String phoneNo = textPhoneNo.getText().toString();
+				String sms = textSMS.getText().toString();
+
+				try {
+					SmsManager smsManager = SmsManager.getDefault();
+					smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+					Toast.makeText(getApplicationContext(), "SMS Sent!",
+							Toast.LENGTH_LONG).show();
+				} catch (Exception e) {
+					Toast.makeText(getApplicationContext(),
+							"SMS faild, please try again later!",
+							Toast.LENGTH_LONG).show();
+					e.printStackTrace();
+				}
+
+			}
+		});
+		nazad.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openPorukaActivity = new Intent(
+						"com.example.pois24.PORUKA");
+				startActivity(openPorukaActivity);
+			}
+		});
+
+		
+	}
+}
