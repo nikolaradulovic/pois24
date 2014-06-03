74a62f4084d2c3a3cd87e1b2c6cda3b280b94d17
Aracki <aracki.ivan@gmail.com>
2014-06-03 02:31:06 +0200

    .

---
+++ pois24/src/com/example/pois24/PodesavanjaActivity.java
@@ -0,0 +1,28 @@
+package com.example.pois24;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PodesavanjaActivity extends ActionBarActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// TODO Auto-generated method stub
+//		return super.onCreateOptionsMenu(menu);
+//	}
+//	
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// TODO Auto-generated method stub
+//		return super.onOptionsItemSelected(item);
+//	}
+	
+}
